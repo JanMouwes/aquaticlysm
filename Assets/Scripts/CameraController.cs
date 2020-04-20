3d6114fac750359faabf62f0684b9fb5a4123fee
bobtaekema <>
2020-04-20 14:17:43 +0200

    made small tweaks

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,52 +6,77 @@ public class CameraController : MonoBehaviour
     public GameObject camera;
 
     public float movementSpeed;
-    public float zoomSpeed;
     public float rotateSpeed;
 
+    public float minRotation = -45;
+    public float maxRotation = 45;
+    public float rotationHeight;
+
+    public float minHeight;
+    public float maxHeight;
+
+    private float _zoom;
     private void Start()
     {
-        movementSpeed = 15.0f;
-        zoomSpeed = 15.0f;
-        rotateSpeed = 30.0f;
+        
     }
 
     private void Update()
     {
+        // X, Z axis movement of CameraController
         transform.Translate(
             Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime,
             0,
             Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime
         );
-        float zoom = -Input.GetAxis("Mouse ScrollWheel");
+
+        // Zoom camera based on scroll wheel input
+        Zoom();
+
+        // Only rotate camera when within rotationheight range
+        if (transform.position.y <= rotationHeight)
+        {
+            ZoomRotate();
+        }
+
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    private void Zoom()
+    {
+        _zoom = -Input.GetAxis("Mouse ScrollWheel");
         transform.Translate(
-            0,
-            Mathf.Clamp(
-                zoom * zoomSpeed * 100f * Time.deltaTime, 
-                -30f, 30f
-                ),
-            0
+            0, _zoom * rotateSpeed * 100f * Time.deltaTime, 0
         );
-        camera.transform.Rotate(Vector3.right, zoom * rotateSpeed);
 
         transform.position = new Vector3(transform.position.x,
             Mathf.Clamp(
                 transform.position.y,
-                1.0f,
-                15f
+                minHeight,
+                maxHeight
             ),
             transform.position.z
         );
 
-        float minRotation = -45;
-        float maxRotation = 45;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    private void ZoomRotate()
+    {
+        camera.transform.Rotate(Vector3.right, _zoom * rotateSpeed);
+       
         Vector3 currentRotation = camera.transform.localRotation.eulerAngles;
-        if (currentRotation.x > maxRotation + rotateSpeed)
+        if (currentRotation.x > maxRotation + (maxRotation - minRotation) / (rotationHeight - minHeight))
         {
             currentRotation.x = 0f;
         }
         currentRotation.x = Mathf.Clamp(currentRotation.x, minRotation, maxRotation);
         camera.transform.localRotation = Quaternion.Euler(currentRotation);
 
+        Debug.Log(currentRotation.x);
     }
 }
