b0191814eaeeac164f7d7f407349a05ccee1cff0
bobtaekema <>
2020-04-20 10:06:06 +0200

    Finished

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float movementSpeed;
+    void Start()
+    {
+        movementSpeed = 8.0f;
+    }
+
+    void Update()
+    {
+        transform.Translate(
+            Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime , 
+            0, 
+            Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime
+            );
+
+    }
+
+}
