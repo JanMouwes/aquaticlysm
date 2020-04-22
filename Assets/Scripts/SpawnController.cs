48db3af7e8935ab1e7d91fc118a02f399c9eef9c
Sven Vogel <svenvogel1998@gmail.com>
2020-04-22 10:47:14 +0200

    update spawn/destroy gameObject

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class SpawnController : MonoBehaviour
 {
-    public List<GameObject> entities;
-    public GameObject PrefabGameObject;
-
+    private List<GameObject> entities;
     private void Awake()
     {
         entities = new List<GameObject>();
     }
 
-    // Start is called before the first frame update
-    private void Start()
+    private int _index = -1;
+    public int Spawn(GameObject gameObject, Vector3 spawnPosition, Quaternion rotation)
     {
-        
+        _index++;
+        string Objectname = "test " + _index;
+        entities.Add(Instantiate(gameObject, spawnPosition, rotation));
+        entities[_index].name = Objectname;
+        return _index;
     }
 
-    // Update is called once per frame
-    private void Update()
+    public GameObject GetGameObject(int Index){
+        return entities[Index];
+    }
+
+    public int Spawn(GameObject gameObject, Vector3 spawnPosition){
+        return Spawn(gameObject,spawnPosition,new Quaternion(0, 0, 0, 1));
+    }
+
+    public void DestroyGameObject(int Index)
     {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Instantiate(PrefabGameObject, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 1));
-
-            entities.Add(PrefabGameObject);
-        }
-
-        if (entities.Count > 0)
-        {
-            entities[0].transform.Translate(0, 1, 0);
-        }
+        Destroy(entities[Index]);
+        //entities.RemoveAt(Index);
     }
 }
---
+++ Assets/Scripts/SpawnObjectDemo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectDemo : MonoBehaviour
+{
+    public GameObject a;
+    private GameObject variableForPrefab;
+    public SpawnController script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        variableForPrefab = (GameObject)Resources.Load("CapsuleJesus", typeof(GameObject));
+        script = a.GetComponent<SpawnController>();
+    }
+
+    // Update is called once per frame
+    private List<int> test = new List<int>();
+
+    public GameObject PrefabGameObject;
+  
+    private int i =0;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.V))
+        {
+            test.Add(script.Spawn(variableForPrefab, new Vector3(0,0,0)));
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            for (int i = 0; i < test.Count; i++)
+            {
+                script.DestroyGameObject(test[i]);
+            }
+            test.Clear();
+        }
+    }
+}
