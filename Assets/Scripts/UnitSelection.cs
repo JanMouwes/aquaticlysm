806ee046cdbd15ebc2432b55ad2d19c14f54142c
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-04-23 11:28:24 +0200

    Created a selection box within the canvas

--- Assets/Scripts/UnitSelection.cs
+++ Assets/Scripts/UnitSelection.cs
@@ -1,57 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitSelection : MonoBehaviour
 {
+    [Tooltip("The canvas of the selection box")]
+    public Canvas canvas;
 
+    [Tooltip("The image that will represent the selection box.")]
     public RectTransform selectionBox;
-    private Vector2 startPos;
+
+    // The start position of the mouseclick inside of the canvas
+    private Vector2 startScreenPosition;
+
+    // The position where the raycast hits
     private RaycastHit hit;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize the selection box
+        if (selectionBox != null)
+        {
+            // We need to reset anchors and pivot to ensure proper positioning
+            selectionBox.pivot = Vector2.one * .5f;
+            selectionBox.anchorMin = Vector2.one * .5f;
+            selectionBox.anchorMax = Vector2.one * .5f;
+            selectionBox.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        // Use a raycast to register the position of the mouse click and set the agents new destination
-        // Range for the raycast is set to 100
-        if (Input.GetButtonDown("LeftMouseButton") && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000))
-            startPos = hit.point;
+        // Return true when the left mouse button is pressed and the raycast (range 300) hits the floor
+        if (Input.GetButtonDown("LeftMouseButton") && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 300))
+        {
+            startScreenPosition = Input.mousePosition;
+            Debug.Log(Input.mousePosition);
+            Debug.Log(hit.point);
+            Debug.Log(hit);
+        }
 
-        // Mouse held down
+        // Return true when the left mouse button is held down and the raycast (range 300) hits the floor
         if (Input.GetButton("LeftMouseButton") && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000))
-            UpdateSelectionBox(hit.point);
+            UpdateSelectionBox(Input.mousePosition);
 
+        // Return true when the left mouse button is released
         if (Input.GetButtonUp("LeftMouseButton"))
-            ReleaseSelectionBox();
+            selectionBox.gameObject.SetActive(false);
     }
 
-    // called when we are creating a selection box
-    void UpdateSelectionBox(Vector2 curMousePos)
+    /// <summary>
+    /// Update the shape of the selection box inside of the canvas
+    /// </summary>
+    /// <param name="curMousePosition">The current mouse position</param>
+    private void UpdateSelectionBox(Vector2 mousePosition)
     {
+        // Make the selection box active if it's not
         if (!selectionBox.gameObject.activeInHierarchy)
             selectionBox.gameObject.SetActive(true);
 
-        float width = curMousePos.x - startPos.x;
-        float height = curMousePos.y - startPos.y;
+        // Create bounds (AABB or just a rectangle if your not familiar with it)
+        Bounds boundingBox = new Bounds();
 
-        selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
-        selectionBox.anchoredPosition = startPos + new Vector2(width / 2, height / 2);
-    }
+        // Set the center of the bounding box between the start position and the mouse position
+        boundingBox.center = Vector2.Lerp(startScreenPosition, mousePosition, 0.5f);
 
-    // called when we release the selection box
-    void ReleaseSelectionBox()
-    {
-        selectionBox.gameObject.SetActive(false);
+        // Set the size of the bounding box
+        boundingBox.size = new Vector2(Mathf.Abs(startScreenPosition.x - mousePosition.x), Mathf.Abs(startScreenPosition.y - mousePosition.y));
 
-        Vector2 min = selectionBox.anchoredPosition - (selectionBox.sizeDelta / 2);
-        Vector2 max = selectionBox.anchoredPosition + (selectionBox.sizeDelta / 2);
+        // Set the position of the selection box
+        selectionBox.position = boundingBox.center;
 
-
+        // Set the size of the selection box
+        selectionBox.sizeDelta = canvas.transform.InverseTransformVector(boundingBox.size);
     }
 }
