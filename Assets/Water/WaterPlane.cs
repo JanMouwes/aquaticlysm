74b0d758ec86ed905db37f2a280326f56a8cdaf9
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-05-07 20:36:59 +0200

    Added comments to the waterplane

--- Assets/Water/WaterPlane.cs
+++ Assets/Water/WaterPlane.cs
@@ -6,26 +6,35 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class WaterPlane : MonoBehaviour
 {
+    // Plane size
     public int xSize, ySize;
 
+    // Mesh filter
     private MeshFilter filter;
 
+    // Awake is called before Start
     void Awake()
     {
+        // Initialize filter
         filter = GetComponent<MeshFilter>();
         filter.mesh = GenerateMesh();
-        
     }
 
+    /// <summary>
+    /// Procedural plane generation
+    /// </summary>
     private Mesh GenerateMesh()
     {
+        // Create mesh
         Mesh mesh = new Mesh();
         mesh.name = "Procedural Water Grid";
 
+        // Create mesh data
         Vector3[] vertices = new Vector3[(xSize + 1) * (ySize + 1)];
         Vector3[] normals = new Vector3[(xSize + 1) * (ySize + 1)];
         Vector2[] uvs = new Vector2[(xSize + 1) * (ySize + 1)];
 
+        // Generate vetices, normals and the uv
         for (int i = 0, x = 0; x < xSize; x++)
             for (int y = 0; y < ySize; y++, i++)
             {
@@ -36,6 +45,7 @@ public class WaterPlane : MonoBehaviour
 
         int[] triangles = new int[xSize * ySize * 6];
 
+        // Create two triangles for every square of vertices
         for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
             for (int x = 0; x < xSize; x++, ti += 6, vi++)
             {
@@ -45,16 +55,11 @@ public class WaterPlane : MonoBehaviour
                 triangles[ti + 5] = vi + xSize + 2;
             }
 
+        // Set and return the mesh
         mesh.SetVertices(vertices);
         mesh.SetNormals(normals);
         mesh.SetUVs(0, uvs);
         mesh.SetTriangles(triangles, 0);
         return mesh;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
