37a9e78075b070ac7c1b09f368b2321c3bb326e2
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-05-08 12:23:00 +0200

    Fixed the triagles generation and added a prefab

--- Assets/Water/WaterPlane.cs
+++ Assets/Water/WaterPlane.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class WaterPlane : MonoBehaviour
 {
     // Plane size
-    public int xSize, ySize;
+    public int size;
 
     // Mesh filter
     private MeshFilter filter;
@@ -30,30 +30,47 @@ public class WaterPlane : MonoBehaviour
         mesh.name = "Procedural Water Grid";
 
         // Create mesh data
-        Vector3[] vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        Vector3[] normals = new Vector3[(xSize + 1) * (ySize + 1)];
-        Vector2[] uvs = new Vector2[(xSize + 1) * (ySize + 1)];
+        Vector3[] vertices = new Vector3[(size) * (size)];
+        Vector3[] normals = new Vector3[(size) * (size)];
+        Vector2[] uvs = new Vector2[(size) * (size )];
 
         // Generate vetices, normals and the uv
-        for (int i = 0, x = 0; x < xSize; x++)
-            for (int y = 0; y < ySize; y++, i++)
+        for (int i = 0, x = 0; x < size; x++)
+            for (int y = 0; y < size; y++, i++)
             {
-                vertices[i] = new Vector3(-xSize * 0.5f + xSize * (x / (float)xSize), 0 , -ySize * 0.5f + ySize * (y / (float)ySize));
+                vertices[i] = new Vector3(-size * 0.5f + size * (x / (float)size), 0 , -size * 0.5f + size * (y / (float)size));
                 normals[i] = (Vector3.up);
-                uvs[i] = new Vector2(x / (float)xSize, y / (float)ySize);
+                uvs[i] = new Vector2(x / (float)size, y / (float)size);
             }
 
-        int[] triangles = new int[xSize * ySize * 6];
+        int[] triangles = new int[size * size * 6];
+
+        /*
+        // Create two triangles for every square of vertices
+        for (int i = 0, vi = 0, x = 0; x < size; x++, vi++)
+            for (int y = 0; y < size; y++, i += 6, vi++)
+            {
+                triangles[i] = vi;
+                triangles[i + 3] = triangles[i + 2] = vi + 1;
+                triangles[i + 4] = triangles[i + 1] = vi + size;
+                triangles[i + 5] = vi + size + 1;
+            }
+        */
 
         // Create two triangles for every square of vertices
-        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
-            for (int x = 0; x < xSize; x++, ti += 6, vi++)
-            {
-                triangles[ti] = vi;
-                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
-                triangles[ti + 5] = vi + xSize + 2;
-            }
+        for (int i = 0, vi = 0, ti = 0; i <= size * size - size * 2; i++, vi++, ti+=6)
+        {
+            Debug.Log("i: " +  i);
+
+            //Debug.Log(size * size + size);
+            if ((vi + 1) % size == 0)
+                vi++;
+
+            triangles[ti] = vi;
+            triangles[ti + 3] = triangles[ti + 2] = vi + size;
+            triangles[ti + 4] = triangles[ti + 1] = vi + 1;
+            triangles[ti + 5] = vi + size + 1;
+        }
 
         // Set and return the mesh
         mesh.SetVertices(vertices);
