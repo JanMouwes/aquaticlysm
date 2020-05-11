b6d4f369a87f239c244dca2b2d19411594c0c03a
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-05-11 16:13:06 +0200

    Added an actioncontroller

---
+++ Assets/Scripts/ActionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ActionController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("RightMouseButton"))
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                RaycastHit hit;
+
+                // Use a raycast (range 100) to register the position of the mouse click and set the agents new destination
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+                    Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+    }
+}
