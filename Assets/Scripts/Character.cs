2ef2ca41c71b4c957b0bca55e39146aaa23d0eb6
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-05-11 11:50:54 +0200

    Initial Character Class

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,21 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
+using System;
+
+public enum Profession { None, Doctor, Farmer, Fisher }
+public enum Skill { None, Medical, Farming, Fishing}
 
 /// <summary>
 ///     Basescript for agents to determine, initialize and update decisionmaking and needs.
 /// </summary>
 public class Character : MonoBehaviour
 {
-    public NavMeshAgent agent;
-    public ThinkGoal thinkGoal;
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public int Age { get; private set; }
 
-    public float energyLevel = 100;
+    public float energyLevel;
+    public Profession profession;
+    public ushort[] skills;
+    public NavMeshAgent agent;
+    private ThinkGoal _brain;
+
 
     // Start is called before the first frame update
     private void Start()
     {
+        energyLevel = 100;
+        profession = Profession.None;
+        skills = new ushort[System.Enum.GetValues(typeof(Skill)).Length];
+        
         agent = GetComponent<NavMeshAgent>();
-        thinkGoal = new ThinkGoal(this);
+        _brain = new ThinkGoal(this);
     }
 
     // Update is called once per frame
@@ -24,6 +39,6 @@ public class Character : MonoBehaviour
         // Check, that energylevel does not get lower during resting
         energyLevel -= 8 * Time.deltaTime;
 
-        thinkGoal.Process();
+        _brain.Process();
     }
 }
