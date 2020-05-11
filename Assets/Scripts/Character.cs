5ba2ec08eea6a296c15255523752347d0526abb1
bobtaekema <>
2020-05-11 13:50:18 +0200

    Expanded Character class

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,9 +2,8 @@
 using UnityEngine.AI;
 using System.Collections;
 using System;
-
-public enum Profession { None, Doctor, Farmer, Fisher }
-public enum Skill { None, Medical, Farming, Fishing}
+using System.Collections.Generic;
+using System.Linq;
 
 /// <summary>
 ///     Basescript for agents to determine, initialize and update decisionmaking and needs.
@@ -16,19 +15,12 @@ public class Character : MonoBehaviour
     public int Age { get; private set; }
 
     public float energyLevel;
-    public Profession profession;
-    public ushort[] skills;
     public NavMeshAgent agent;
     private ThinkGoal _brain;
-
-
+    
     // Start is called before the first frame update
     private void Start()
     {
-        energyLevel = 100;
-        profession = Profession.None;
-        skills = new ushort[System.Enum.GetValues(typeof(Skill)).Length];
-        
         agent = GetComponent<NavMeshAgent>();
         _brain = new ThinkGoal(this);
     }
