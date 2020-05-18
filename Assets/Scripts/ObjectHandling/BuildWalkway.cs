a0a5deb40aa53252b9019ec99f49e51d9ee68817
JanMouwes <jan.mouwes@gmail.com>
2020-05-18 10:57:31 +0200

    Added review changes

--- Assets/Scripts/ObjectHandling/BuildWalkway.cs
+++ Assets/Scripts/ObjectHandling/BuildWalkway.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.EventSystems;
-using UnityEngine.Serialization;
 
 /// <summary>
 /// Class for building walkways
@@ -11,20 +9,10 @@ using UnityEngine.Serialization;
 public class BuildWalkway : MonoBehaviour
 {
     private GameObject _currentEntity;
+    private Outline _outline;
     private NavMeshSurface _navMeshSurface;
 
     public GameObject walkwayPrefab;
-
-    /// <summary>
-    /// Material to highlight current entity
-    /// </summary>
-    public Material selectedMaterial;
-
-    private Outline _outline;
-    /// <summary>
-    /// Actual material when entity is set down
-    /// </summary>
-    public Material normalMaterial;
 
     // Start is called before the first frame update
     private void Awake()
@@ -47,23 +35,23 @@ public class BuildWalkway : MonoBehaviour
     private void UpdateCreateEntity()
     {
         if (!Input.GetButtonDown("CreateNewObject") || this._currentEntity != null) return;
-        
+
         // Check, if clicking on UI or on the game world
         if (!EventSystem.current.IsPointerOverGameObject())
         {
             // Get mouse position
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
             {
-                _currentEntity = PrefabInstanceManager.Instance.Spawn(
+                this._currentEntity = PrefabInstanceManager.Instance.Spawn(
                     this.walkwayPrefab,
                     new Vector3(hit.point.x, 0, hit.point.z),
                     Quaternion.Euler(0, 90, 0)
                 );
-                _outline = _currentEntity.GetComponent<Outline>();
-                _outline.enabled = true;
+                this._outline = this._currentEntity.GetComponent<Outline>();
+                this._outline.enabled = true;
             }
         }
-        
+
         this._currentEntity.transform.parent = this.transform;
     }
 
@@ -76,14 +64,8 @@ public class BuildWalkway : MonoBehaviour
 
         // Keep dock following the mouse.
         UpdateDockPosition();
-        if (DoesEntityCollide())
-        {
-            _outline.OutlineColor= Color.yellow;
-        }
-        else
-        {
-            _outline.OutlineColor= Color.red;
-        }
+
+        this._outline.OutlineColor = DoesEntityCollide() ? Color.red : Color.yellow;
 
         // Pressing escape destroy dock not yet placed.
         if (Input.GetButtonDown("Cancel"))
@@ -106,9 +88,9 @@ public class BuildWalkway : MonoBehaviour
         // After checking, if the position is available for building, build a dock pressing U.
         if (Input.GetButtonDown("KeyBuildHere"))
         {
-            if (DoesEntityCollide())
+            if (!DoesEntityCollide())
             {
-                SwitchToMaterial(this.normalMaterial);
+                this._outline.enabled = false;
                 this._currentEntity = null;
                 this._navMeshSurface.BuildNavMesh();
             }
@@ -130,18 +112,6 @@ public class BuildWalkway : MonoBehaviour
     }
 
     /// <summary>
-    /// Emphasize with an orange highlighter, if currently raycasted gameObject is selected or not.
-    /// </summary>
-    /// <param name="material">Material to switch to</param>
-    private void SwitchToMaterial(Material material)
-    {
-        //this._currentEntity.GetComponent<Outline>().enabled = !_currentEntity.GetComponent<Outline>().enabled;
-
-        // If gameObject is created just now, use highlighted material.
-        //gameObjectRenderer.material = material;
-    }
-
-    /// <summary>
     /// Get all gameObjects colliding with the mouse position to check, if its possible to build here.
     /// </summary>
     /// <returns></returns>
@@ -152,6 +122,6 @@ public class BuildWalkway : MonoBehaviour
         Bounds bounds = this._currentEntity.GetComponent<BoxCollider>().bounds;
 
         return allWalkways.Where(walkway => walkway != this._currentEntity)
-                          .All(walkway => !walkway.GetComponent<BoxCollider>().bounds.Intersects(bounds));
+                          .Any(walkway => walkway.GetComponent<BoxCollider>().bounds.Intersects(bounds));
     }
 }
