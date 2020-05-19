51962bd3827e9ce01b7fb6fd13166756db56a391
Meri Eskelinen <merituulie@hotmail.fi>
2020-05-19 15:45:08 +0200

    restructureed selectcontroller code

--- Assets/Scripts/Actions/SelectionController.cs
+++ Assets/Scripts/Actions/SelectionController.cs
@@ -47,23 +47,7 @@ public class SelectionController : MonoBehaviour
         if (Input.GetButtonDown("LeftMouseButton") && 
             MouseUtil.TryRaycastAtMousePosition(300, out raycastHit))
         {
-            // Clear all selected items.
-            ClearSelected();
-
-            // Select a single unit.
-            Selectable s = raycastHit.collider.GetComponentInParent<Selectable>();
-            if (s != null)
-            {
-                s.Selected = true;
-                selectedEntities.Add(s);
-            }
-
-            // Register the mouse coordinates within the canvas.
-            startScreenPosition = Input.mousePosition;
-
-            // Set the pos of the raycasthit.
-            startWorldSpace.x = raycastHit.point.x;
-            startWorldSpace.y = raycastHit.point.z;
+            SelectSingleUnit(raycastHit);
         }
 
         // Return true when the left mouse button is held down and the raycast (range 1000) hits the floor.
@@ -81,23 +65,49 @@ public class SelectionController : MonoBehaviour
         // Return true when the left mouse button is released.
         if (Input.GetButtonUp("LeftMouseButton"))
         {
-            // Close the selection box.
-            selectionBox.gameObject.SetActive(false);
+            SelectMultipleUnits();
+        }
+    }
 
-            // Create a bounding box with the positions in the worldspace.
-            Bounds boundingBox = CreateBoundingBox(startWorldSpace, endWorldSpace);
+    private void SelectSingleUnit(RaycastHit raycastHit)
+    {
+        // Clear all selected items.
+        ClearSelected();
 
-            // Foreach through all selectables and check if it's inside of the bounding box and a unit or boat.
-            foreach (Selectable selectable in selectables)
+        // Select a single unit.
+        Selectable s = raycastHit.collider.GetComponentInParent<Selectable>();
+        if (s != null)
+        {
+            s.Selected = true;
+            selectedEntities.Add(s);
+        }
+
+        // Register the mouse coordinates within the canvas.
+        startScreenPosition = Input.mousePosition;
+
+        // Set the pos of the raycasthit.
+        startWorldSpace.x = raycastHit.point.x;
+        startWorldSpace.y = raycastHit.point.z;
+    }
+
+    private void SelectMultipleUnits()
+    {
+        // Close the selection box.
+        selectionBox.gameObject.SetActive(false);
+
+        // Create a bounding box with the positions in the worldspace.
+        Bounds boundingBox = CreateBoundingBox(startWorldSpace, endWorldSpace);
+
+        // Foreach through all selectables and check if it's inside of the bounding box and a unit or boat.
+        foreach (Selectable selectable in selectables)
+        {
+            if (selectable is Character gameObject)
             {
-                if (selectable is Character gameObject)
+                if (boundingBox.Contains(new Vector2(gameObject.transform.position.x, gameObject.transform.position.z)))
                 {
-                    if(boundingBox.Contains(new Vector2(gameObject.transform.position.x, gameObject.transform.position.z)))
-                    {
-                        selectable.Selected = true;
-                        
-                        selectedEntities.Add(selectable);
-                    }
+                    selectable.Selected = true;
+
+                    selectedEntities.Add(selectable);
                 }
             }
         }
