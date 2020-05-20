95f1f90e9d3d24f5f18b3c691b60ff9d5b58a131
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-05-20 15:23:06 +0200

    Added a single unit test

---
+++ Assets/Tests/PlayMode/GlobalStateMachineTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GlobalStateMachineTest
+    {
+        [UnityTest]
+        public IEnumerator WhenStateChanged_ShouldChangeState()
+        {
+            //Arrange
+            SceneManager.LoadScene("Scenes/LocalSea");
+            IState currentState = null;
+            IState state = new Build();
+
+            //Act
+            GlobalStateMachine.instance.StateChanged += (IState s) => currentState = s;
+            GlobalStateMachine.instance.ChangeState(state);
+
+            yield return null;
+
+            //Assert
+            Assert.AreEqual(state, currentState);
+        }
+    }
+}
