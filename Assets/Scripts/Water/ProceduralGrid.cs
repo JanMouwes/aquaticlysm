c8f3852362e53c87675e99d4adf0df0f78f0a01b
JanMouwes <jan.mouwes@gmail.com>
2020-05-22 14:29:01 +0200

    Cleaned up ProceduralGrid

--- Assets/Scripts/Water/ProceduralGrid.cs
+++ Assets/Scripts/Water/ProceduralGrid.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralGrid : MonoBehaviour
 {
-    [Tooltip("Grid size (x and y).")]
-    public int size;
+    [Tooltip("Grid size (x and y).")] public int size;
 
     // Mesh filter
     private MeshFilter _filter;
@@ -20,37 +19,63 @@ public class ProceduralGrid : MonoBehaviour
 
         // Initialize filter
         _filter = GetComponent<MeshFilter>();
-        _filter.mesh = GenerateMesh();
+        _filter.mesh = GenerateMesh(this.size, -size * 0.5f);
     }
 
     /// <summary>
     /// Procedural plane generation
     /// </summary>
-    private Mesh GenerateMesh()
+    private static Mesh GenerateMesh(int size, float offset)
     {
         // Create mesh
-        Mesh mesh = new Mesh();
-        mesh.name = "Procedural Grid";
+        Mesh mesh = new Mesh {name = "Procedural Grid"};
 
         // Create mesh data
         Vector3[] vertices = new Vector3[size * size];
         Vector3[] normals = new Vector3[size * size];
         Vector2[] uvs = new Vector2[size * size];
-        float negativeOffset = -size * 0.5f;
 
         // Generate vetices, normals and the uv
-        for (int i = 0, x = 0; x < size; x++)
+        for (
+            int i = 0,
+                x = 0;
+            x < size;
+            x++
+        )
             for (int y = 0; y < size; y++, i++)
             {
-                vertices[i] = new Vector3(negativeOffset + size * (x / (float)size), 0 , negativeOffset + size * (y / (float)size));
+                vertices[i] = GetVectorForPositionIndex(x, y, offset);
                 normals[i] = Vector3.up;
-                uvs[i] = new Vector2(x / (float)size, y / (float)size);
+                uvs[i] = new Vector2(x / (float) size, y / (float) size);
             }
 
+        int[] triangles = GenerateTriangles(size);
+
+        // Set and return the mesh
+        mesh.SetVertices(vertices);
+        mesh.SetNormals(normals);
+        mesh.SetUVs(0, uvs);
+        mesh.SetTriangles(triangles, 0);
+
+        return mesh;
+    }
+
+    public static Vector3 GetVectorForPositionIndex(int xIndex, int yIndex, float offset)
+    {
+        return new Vector3(offset + xIndex, 0, offset + yIndex);
+    }
+
+    public static int[] GenerateTriangles(int size)
+    {
         int[] triangles = new int[size * size * 6];
+        int limit = size * size - size * 2;
 
         // Create two triangles for every square of vertices
-        for (int i = 0, vi = 0, ti = 0; i <= size * size - size * 2; i++, vi++, ti+=6)
+        for (int i = 0,
+                 vi = 0,
+                 ti = 0;
+            i <= limit;
+            i++, vi++, ti += 6)
         {
             if ((vi + 1) % size == 0)
                 vi++;
@@ -61,11 +86,6 @@ public class ProceduralGrid : MonoBehaviour
             triangles[ti + 5] = vi + size + 1;
         }
 
-        // Set and return the mesh
-        mesh.SetVertices(vertices);
-        mesh.SetNormals(normals);
-        mesh.SetUVs(0, uvs);
-        mesh.SetTriangles(triangles, 0);
-        return mesh;
+        return triangles;
     }
 }
