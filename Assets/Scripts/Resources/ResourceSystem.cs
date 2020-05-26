ea553c26f02414636a480b9a780f53317b6e43f0
BobbyDropTables <45656876+BobbyDropTables@users.noreply.github.com>
2020-05-26 13:51:14 +0200

    Added ResourceSystem

---
+++ Assets/Scripts/Resources/ResourceSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Resources
+{
+    public class ResourceSystem : MonoBehaviour
+    {
+        private ResourceManager _instance;
+        private void Start()
+        {
+            _instance = ResourceManager.Instance;
+            
+            InitResources();
+        }
+
+        private void InitResources()
+        {
+            _instance.AddResourceType("wood", 100);
+            _instance.AddResourceType("steel", 100);
+            _instance.AddResourceType("plastic", 100);
+            _instance.AddResourceType("water", 100);
+        }
+        
+        
+    }
+}
