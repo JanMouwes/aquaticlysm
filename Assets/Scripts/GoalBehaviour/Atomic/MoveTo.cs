bdfa05c2d61d12c8db8503d14e4fdae039043bc2
JanMouwes <jan.mouwes@gmail.com>
2020-05-26 10:28:31 +0200

    Fixed MoveTo-review comment

--- Assets/Scripts/GoalBehaviour/Atomic/MoveTo.cs
+++ Assets/Scripts/GoalBehaviour/Atomic/MoveTo.cs
@@ -8,10 +8,10 @@ public class MoveTo : IGoal
     private GameObject _owner;
     public GoalStatus Status { get; private set; }
     public string Name { get; private set; }
-    public float NearRange  { get; set; }
+    public float NearRange { get; set; }
 
     private Vector3 _previousPosition;
-    
+
     // Target position.
     private readonly Vector3 _target;
 
@@ -22,12 +22,21 @@ public class MoveTo : IGoal
         _target = position;
         NearRange = 2f;
     }
-    
+
     public void Activate()
     {
         // Set destination.
         _owner.GetComponent<NavMeshAgent>().destination = _target;
-        Status = GoalStatus.Active;
+
+
+        NavMeshPath path = new NavMeshPath();
+
+        bool success = NavMesh.CalculatePath(_owner.transform.position, _target,
+                                             _owner.GetComponent<NavMeshAgent>().areaMask, path);
+
+        bool hasFailed = !success || path.status != NavMeshPathStatus.PathComplete;
+
+        this.Status = hasFailed ? GoalStatus.Failed : GoalStatus.Active;
     }
 
     public GoalStatus Process()
@@ -36,23 +45,10 @@ public class MoveTo : IGoal
             Activate();
 
         // Checks if the Owner arrived at the target.
-        if (Vector3.Distance(_owner.transform.position, _target) < NearRange)
-            Terminate();
+        if (Vector3.Distance(_owner.transform.position, _target) < NearRange) { Status = GoalStatus.Completed; }
 
-        NavMeshPath path = new NavMeshPath();
-
-        bool success = NavMesh.CalculatePath(_owner.transform.position, _target,
-                                             _owner.GetComponent<NavMeshAgent>().areaMask, path);
-        if (!success || path.status != NavMeshPathStatus.PathComplete)
-        {
-            Status = GoalStatus.Failed;
-        }
-        
         return Status;
     }
 
-    public void Terminate()
-    {
-        Status = GoalStatus.Completed;
-    }
+    public void Terminate() { }
 }
