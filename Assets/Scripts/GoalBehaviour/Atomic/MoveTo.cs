f3a1947983c1bf5c88f1cbf341643fcc312f5d9b
Sven Vogel <svenvogel1998@gmail.com>
2020-05-26 12:04:21 +0200

    Merge branch 'develop' into #48-Navigation-for-ships

--- Assets/Scripts/GoalBehaviour/Atomic/MoveTo.cs
+++ Assets/Scripts/GoalBehaviour/Atomic/MoveTo.cs
@@ -8,8 +8,10 @@ public class MoveTo : IGoal
     private GameObject _owner;
     public GoalStatus Status { get; private set; }
     public string Name { get; private set; }
-    public float NearRange  { get; set; }
-    
+    public float NearRange { get; set; }
+
+    private Vector3 _previousPosition;
+
     // Target position.
     private readonly Vector3 _target;
 
@@ -20,12 +22,21 @@ public class MoveTo : IGoal
         _target = position;
         NearRange = 2f;
     }
-    
+
     public void Activate()
     {
         // Set destination.
         _owner.GetComponent<NavMeshAgent>().destination = _target;
-        Status = GoalStatus.Active;
+
+
+        NavMeshPath path = new NavMeshPath();
+
+        bool success = NavMesh.CalculatePath(_owner.transform.position, _target,
+                                             _owner.GetComponent<NavMeshAgent>().areaMask, path);
+
+        bool hasFailed = !success || path.status != NavMeshPathStatus.PathComplete;
+
+        this.Status = hasFailed ? GoalStatus.Failed : GoalStatus.Active;
     }
 
     public GoalStatus Process()
@@ -34,14 +45,10 @@ public class MoveTo : IGoal
             Activate();
 
         // Checks if the Owner arrived at the target.
-        if (Vector3.Distance(_owner.transform.position, _target) < NearRange)
-            Terminate();
+        if (Vector3.Distance(_owner.transform.position, _target) < NearRange) { Status = GoalStatus.Completed; }
         
         return Status;
     }
 
-    public void Terminate()
-    {
-        Status = GoalStatus.Completed;
-    }
+    public void Terminate() { }
 }
--- Assets/Scripts/GoalBehaviour/Composite/Think.cs
+++ Assets/Scripts/GoalBehaviour/Composite/Think.cs
@@ -1,13 +1,11 @@
-﻿using UnityEngine;
-
-/// <summary>
+﻿/// <summary>
 ///     Highest level of goal managing, makes decisions between strategies to
 ///     fulfill agents pressing needs.
 /// </summary>
 public class Think : CompositeGoal
 {
     private Character _owner;
-    
+
     public Think(Character owner)
     {
         Name = "Think";
@@ -22,14 +20,10 @@ public class Think : CompositeGoal
     /// <summary>
     ///     Checks if there is a need for a new goal.
     /// </summary>
-    public void Evaluate()
+    public void FindSubGoal()
     {
         // Check if there is need for resting and also that the agent is not currently taking care of it.
-        if (needsRest(_owner.energyLevel))
-        {
-            Vector3 restLocation = GameObject.FindGameObjectWithTag("Rest").transform.position;
-            AddSubGoal(new Rest(_owner));
-        }
+        if (needsRest(_owner.energyLevel)) { AddSubGoal(new Rest(_owner)); }
     }
 
     public override GoalStatus Process()
@@ -37,23 +31,12 @@ public class Think : CompositeGoal
         if (Status == GoalStatus.Inactive)
             Activate();
 
-        if (SubGoals.Count == 0)
-        {
-            Evaluate();
-        }
-        else
-        {
-            // Process new subgoal
-            SubGoals.Peek().Process();
-            RemoveCompletedSubgoals();
-        }
+        if (SubGoals.Count == 0) { FindSubGoal(); }
 
-        return Status;
+        return base.Process();
     }
 
-    public override void Terminate()
-    {
-    }
+    public override void Terminate() { }
 
     /// <summary>
     /// Check, if an entity needs rest.
