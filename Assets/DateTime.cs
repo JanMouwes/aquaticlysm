cc5bd14d9367cf607fc0a5214e7be91041d1772a
BobbyDropTables <45656876+BobbyDropTables@users.noreply.github.com>
2020-05-27 09:54:49 +0200

    Minor code cleanup

--- Assets/DateTime.cs
+++ Assets/DateTime.cs
@@ -3,23 +3,23 @@ using UnityEngine.UI;
 
 public class DateTime : MonoBehaviour
 {
-    public Text DateTimeText;
+    public Text dateTimeText;
 
     private float _gameTime;
     private string _timeText;
     private int _dayCounter = 1;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _gameTime = 100.0f;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
 
-        if (dayHasPassed(_gameTime))
+        if (DayHasPassed(_gameTime))
         {
             _gameTime = 100.0f;
             _dayCounter++;
@@ -28,8 +28,8 @@ public class DateTime : MonoBehaviour
         _gameTime -= Time.deltaTime;
         _timeText = _gameTime.ToString();
 
-        DateTimeText.text = "DAY " + _dayCounter;
+        dateTimeText.text = "DAY " + _dayCounter;
     }
 
-    private static bool dayHasPassed(float passedGameTime) => passedGameTime <= 0f;
+    private static bool DayHasPassed(float passedGameTime) => passedGameTime <= 0f;
 }
