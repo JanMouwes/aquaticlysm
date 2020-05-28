8f9ab62c1cacb38a87c4443508c154c6ecf2747a
Ossama Sybesma <ossama_sybesma@hotmail.com>
2020-05-28 12:50:17 +0200

    Made the base for expeditions

---
+++ Assets/Scripts/GoalBehaviour/Atomic/Amass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class Amass : IGoal
+{
+    private GameObject _owner;
+
+    public GoalStatus Status { get; private set; }
+
+    public string Name { get; private set; }
+
+    public Amass(GameObject owner, float duration)
+    {
+        _owner = owner;
+
+    }
+
+    public void Activate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public GoalStatus Process()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Terminate()
+    {
+        throw new NotImplementedException();
+    }
+}
