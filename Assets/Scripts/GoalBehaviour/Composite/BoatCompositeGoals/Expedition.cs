77e2c06bc9629cf38e7c8d4e55699c9141c0102c
Ossama Sybesma <ossama_sybesma@hotmail.com>
2020-05-28 14:48:54 +0200

    Epedition now uses the dropoff and amass is a bit better too

--- Assets/Scripts/GoalBehaviour/Composite/BoatCompositeGoals/Expedition.cs
+++ Assets/Scripts/GoalBehaviour/Composite/BoatCompositeGoals/Expedition.cs
@@ -17,7 +17,7 @@ public class Expedition : CompositeGoal
     {
         AddSubGoal(new MoveTo(_owner.gameObject, GetRandomOutermostEdgeVector3(_gridSize)));
         AddSubGoal(new Amass(_owner, Random.Range(20f, 50f)));
-        //AddSubGoal(new MoveTo(_owner.gameObject, _returnTarget));
+        AddSubGoal(new DropOff(_owner));
 
         Status = GoalStatus.Active;
     }
