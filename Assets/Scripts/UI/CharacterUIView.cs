0b0b1e79002841ace6386ac44937ad891c4e847c
JanMouwes <jan.mouwes@gmail.com>
2020-05-29 13:45:02 +0200

    - Added comments to CharacterUIView

--- Assets/Scripts/UI/CharacterUIView.cs
+++ Assets/Scripts/UI/CharacterUIView.cs
@@ -15,7 +15,6 @@ namespace UI
 
         public GameObject characterButtonPrefab;
 
-        
         public IEnumerable<Character> Characters => this.characterSystem.CharacterManager.characters;
 
         private void Update()
@@ -23,14 +22,15 @@ namespace UI
             Character[] characters = this.Characters.ToArray();
             IEnumerable<int> ids = characters.Select(character => character.GetInstanceID()).ToArray();
 
+            // Remove characters that were destroyed
             foreach (int uiElementKey in this._characterUiElements.Keys.ToArray().Where(key => !ids.Contains(key)))
             {
                 if (this._characterUiElements.TryGetValue(uiElementKey, out GameObject element)) { Destroy(element); }
 
-                // Remove key not in characters
                 this._characterUiElements.Remove(uiElementKey);
             }
 
+            // Draw character-buttons
             for (int index = 0; index < characters.Length; index++)
             {
                 Character character = characters[index];
@@ -38,35 +38,37 @@ namespace UI
             }
         }
 
+        /// <summary>
+        /// Draws ui-element for a given character
+        /// </summary>
+        /// <param name="character">Character for which to draw a ui element</param>
+        /// <param name="index">Index of this character within the list of characters. Determines element position</param>
         public void Draw(Character character, int index)
         {
-            int SIZE = 50;
+            const int size = 50;
             int characterId = character.GetInstanceID();
 
+            // Add UI element when not present
             if (!this._characterUiElements.TryGetValue(characterId, out GameObject buttonGameObject))
             {
                 buttonGameObject = PrefabInstanceManager.Instance.Spawn(this.characterButtonPrefab, Vector3.zero);
                 this._characterUiElements[characterId] = buttonGameObject;
             }
+
             Image image = buttonGameObject.GetComponent<Image>();
             Button btn = buttonGameObject.GetComponent<Button>();
             CharacterButton charbtn = buttonGameObject.GetComponent<CharacterButton>();
 
-            if (charbtn.Character == null)
-            {
-                charbtn.Character = character;
-            }
-            if (character.GetComponent<Selectable>().Selected)
-            {
-                btn.Select();
-            }
-            
+            if (charbtn.Character == null) { charbtn.Character = character; }
+
+            if (character.GetComponent<Selectable>().Selected) { btn.Select(); }
+
             RectTransform imageTransform = buttonGameObject.GetComponent<RectTransform>();
-            float imageYPos = index * -SIZE;
+            float imageYPos = index * -size;
 
             imageTransform.SetParent(this.gameObject.transform);
             imageTransform.anchoredPosition = new Vector2(80, imageYPos);
-            
+
             image.sprite = character.Portrait;
         }
     }
