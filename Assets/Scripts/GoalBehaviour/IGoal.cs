d761e802ccca1186ac635c2de23b8160c450d16b
Ossama Sybesma <ossama_sybesma@hotmail.com>
2020-06-02 14:49:28 +0200

    Revert "Hotfix double boats scene"

--- Assets/Scripts/GoalBehaviour/IGoal.cs
+++ Assets/Scripts/GoalBehaviour/IGoal.cs
@@ -4,11 +4,13 @@
 public interface IGoal
 {
     // Status to keep a record of the status of the goal
+    Character Owner { get; }
     GoalStatus Status { get; }
     string Name { get; }
-
     void Activate();
+
     GoalStatus Process();
+
     void Terminate();
 }
 
