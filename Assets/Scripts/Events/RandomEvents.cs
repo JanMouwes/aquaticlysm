facc33f1dff082200b041b79f25c7be643c8eb8d
Sven Vogel <svenvogel1998@gmail.com>
2020-06-02 15:42:21 +0200

    Add Scripts

--- Assets/Scripts/Events/RandomEvents.cs
+++ Assets/Scripts/Events/RandomEvents.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventScript : MonoBehaviour
+public class RandomEvents : MonoBehaviour
 {
-    public GameObject backgound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Test());
     }
 
+    IEnumerator Test()
+    {
+        //Wait for 4 seconds
+        yield return new WaitForSeconds(4);
+        Event.Instance.CreateEvent(2);
+        Debug.Log("hit");
+    }
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/Scripts/UI/EventUIView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventUIView : MonoBehaviour
+{
+    public GameObject backgound;
+
+    private String _eventText;
+    private String _eventTitle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Event.Instance.EventCreated += UpdateEventUIView;
+        _eventText = GameObject.Find("EventText").GetComponent<Text>().text;
+        _eventTitle = GameObject.Find("EventTitle").GetComponent<Text>().text;
+        backgound.SetActive(false);
+    }
+
+    void UpdateEventUIView(String Title, String text, buttonstyle buttonstyle)
+    {
+        _eventText = text;
+        _eventTitle = Title;
+        backgound.SetActive(true);
+    }
+}
