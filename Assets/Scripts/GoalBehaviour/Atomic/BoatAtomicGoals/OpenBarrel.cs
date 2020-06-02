06913d3687bd59050c1c4e854a85e0629248b4a6
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-06-02 14:24:00 +0200

    Fetch barrel AI code

---
+++ Assets/Scripts/GoalBehaviour/Atomic/BoatAtomicGoals/OpenBarrel.cs
@@ -0,0 +1,67 @@
+﻿using Resources;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBarrel : IGoal
+{
+    private Boat _owner;
+    public GoalStatus Status { get; private set; }
+    public string Name { get; private set; }
+
+    private float _time;
+    private int _accumulateAmount;
+    public OpenBarrel(Boat owner, float duration)
+    {
+        Name = "AccumulateResources";
+        _owner = owner;
+        _time = duration;
+    }
+
+    public void Activate()
+    {
+        // The random amount of stuff it wil accumulate.
+        _accumulateAmount = (int)Random.Range(0, _owner.maxCarrierAmount);
+
+        Status = GoalStatus.Active;
+    }
+
+    public GoalStatus Process()
+    {
+        if (Status == GoalStatus.Inactive)
+            Activate();
+
+        _time -= Time.deltaTime;
+
+        if (_time <= 0)
+            Terminate();
+
+        return Status;
+    }
+
+    public void Terminate()
+    {
+        AccumulateRandomResource(_owner, _accumulateAmount);
+        ScrapBarrel(_owner);
+
+        Status = GoalStatus.Completed;
+    }
+
+    /// <summary>
+    /// Fills the boat with random resources.
+    /// </summary>
+    /// <param name="boat">The boat where it will save the resources.</param>
+    /// <param name="amount">The total amount of resources.</param>
+    public static void AccumulateRandomResource(Boat boat, int amount)
+    {
+        string type = ResourceManager.Instance.GetRandomType();
+        float resourceAmount = boat.TryGetResourceValue(type);
+        boat.carriedResources[type] = resourceAmount + amount;
+    }
+
+    public static void ScrapBarrel(Boat boat)
+    {
+        float resourceAmount = boat.TryGetResourceValue("metal");
+        boat.carriedResources["metal"] = resourceAmount + 5;
+    }
+}
---
+++ Assets/Scripts/GoalBehaviour/Composite/BoatCompositeGoals/FetchBarrel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FetchBarrel : CompositeGoal
+{
+    private readonly Boat _owner;
+    private readonly Vector3 _target;
+
+    public FetchBarrel(Boat boat, Vector3 target)
+    {
+        Name = "Gather fish";
+        _owner = boat;
+        _target = target;
+    }
+
+    public override void Activate()
+    {
+        Status = GoalStatus.Active;
+
+        // Add subgoals
+        AddSubGoal(new MoveTo(_owner.gameObject, _target, 2f));
+        AddSubGoal(new OpenBarrel(_owner, 2f));
+        AddSubGoal(new DropOff(_owner));
+    }
+
+    public override GoalStatus Process()
+    {
+        if (SubGoals.Any(subGoal => subGoal.Status == GoalStatus.Failed))
+            Status = GoalStatus.Failed;
+
+        return base.Process();
+    }
+
+    public override void Terminate()
+    {
+        Status = GoalStatus.Completed;
+    }
+}
