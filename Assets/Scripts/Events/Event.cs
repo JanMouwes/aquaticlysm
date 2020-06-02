b58f86095fa18da45537e4e611e726ee38d29333
Sven Vogel <svenvogel1998@gmail.com>
2020-06-02 13:28:21 +0200

    Add UI for events

---
+++ Assets/Scripts/Events/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event
+{
+    private String Title;
+    private String Text;
+    
+    public Event(int id)
+    {
+        
+    }
+}
