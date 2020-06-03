dcda2d625797a48cecd47c140fa31b84b6be2a59
Meri Eskelinen <merituulie@hotmail.fi>
2020-06-03 10:21:09 +0200

    added a actionselection script

---
+++ Assets/Scripts/Actions/ActionSelection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionSelection : MonoBehaviour
+{ 
+    public GameObject ActionPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActionPanel = GameObject.Find("ActionPanel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
