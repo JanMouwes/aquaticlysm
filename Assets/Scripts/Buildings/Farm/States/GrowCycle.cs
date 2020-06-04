03cc68dadf612eb2ec980139484d748e87cbfb37
Ossama Sijbesma <ossama_sybesma@hotmail.com>
2020-06-04 10:17:13 +0200

    Growth Cycle

---
+++ Assets/Scripts/Buildings/Farm/States/GrowCycle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Buildings.Farm
+{
+    public class GrowCycle : IFarmingState
+    {
+        private GameObject _growthPhase;
+        private float _time;
+        private int _phase;
+
+        private string[] _growthPhases = {
+            "GrowthPhase1",
+            "GrowthPhase2",
+            "GrowthPhase3",
+            "GrowthPhase4",
+            "GrowthPhase5",
+        };
+
+        public void Execute(Farm owner)
+        {
+            _time -= Time.deltaTime;
+
+            if (_time <= 0)
+                if (_phase == 4)
+                    owner.ChangeState(new CropsGrown());
+                else
+                    ChangePhase(owner);
+        }
+
+        public void Start(Farm owner)
+        {
+            _phase = 1;
+            _time = 5f;
+            _growthPhase = owner.transform.Find(_growthPhases[_phase]).gameObject;
+            _growthPhase.SetActive(true);
+        }
+
+        public void Stop(Farm owner)
+        {
+        }
+
+        private void ChangePhase(Farm owner)
+        {
+            _phase++;
+            _time = 10f;
+            _growthPhase.SetActive(false);
+            _growthPhase = owner.transform.Find(_growthPhases[_phase]).gameObject;
+            _growthPhase.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/Buildings/Farm/States/SoilEmpty.cs
+++ Assets/Scripts/Buildings/Farm/States/SoilEmpty.cs
@@ -4,14 +4,10 @@ namespace Buildings.Farm
 {
     public class SoilEmpty : IFarmingState
     {
-
         private Character _farmer;
-        private GameObject _growthPhase;
         
         public void Start(Farm owner)
         {
-            Transform trans = owner.transform;
-            _growthPhase = trans.Find("GrowthPhase1").gameObject;
         }
 
         public void Execute(Farm owner)
@@ -30,7 +26,6 @@ namespace Buildings.Farm
 
         public void Stop(Farm owner)
         {
-            _growthPhase.SetActive(true);
         }
     }
 }
