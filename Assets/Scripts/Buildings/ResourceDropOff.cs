60a24dcd0b35636ff97bcca102f128138282931c
JanMouwes <jan.mouwes@gmail.com>
2020-06-05 10:58:08 +0200

    Added resource drop off component

---
+++ Assets/Scripts/Buildings/ResourceDropOff.cs
@@ -0,0 +1,11 @@
+using Resources;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Buildings
+{
+    public class ResourceDropOff : MonoBehaviour
+    {
+        public ResourceManager resourceManager;
+    }
+}
