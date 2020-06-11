53c2ce1457413934533b757eea3499b9bce2ae54
BobbyDropTables <45656876+BobbyDropTables@users.noreply.github.com>
2020-06-11 14:43:19 +0200

    Change MoveTo to MoveToAnim for Villagers

--- Assets/Scripts/GoalBehaviour/Composite/SowSeedsAtFarm.cs
+++ Assets/Scripts/GoalBehaviour/Composite/SowSeedsAtFarm.cs
@@ -31,7 +31,7 @@ namespace GoalBehaviour.Composite
             }
             
             Status = GoalStatus.Active;
-            AddSubGoal(new MoveTo(_owner.gameObject, _target, 2f));
+            AddSubGoal(new MoveToAnim(_owner.gameObject, _target, 2f));
             AddSubGoal(new SowSeeds(_owner, _farm, 2f));
         }
 
